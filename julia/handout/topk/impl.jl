17244eb4ef1fe9ff30b872bb9d1f015a9f523117
TheVeryDarkness <3266343194@qq.com>
2025-01-23 02:34:42 +0800

    Update

--- julia/handout/topk/impl.jl
+++ julia/handout/topk/impl.jl
@@ -1,6 +1,20 @@
 # 原子类型定义，用于线程安全的操作
 mutable struct Atomic{T}; @atomic x::T; end
 
+function topk_part(data::AbstractVector{T}, k::Int64, start::Int64) where T
+    map(partialsortperm(data, 1:k, rev=true)) do i
+        i + start
+    end
+end
+
 @inbounds function topk(data::AbstractVector{T}, k) where T
-    return ans
+    chunk_size = length(data) ÷ Threads.nthreads()
+    chunks = enumerate(Iterators.partition(data, chunk_size))
+    tasks = map(chunks) do (i, chunk)
+        Threads.@spawn topk_part(chunk, k, i * chunk_size)
+    end
+    chunk_indices = reduce(vcat, fetch.(tasks))
+    partialsort(chunk_indices, 1:k, rev=true, by=x->data[x])
+
+    # partialsortperm(data, 1:k, rev=true)
 end
