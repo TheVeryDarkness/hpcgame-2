2008469563083349ecd330aa8938169a6f5811c0
TheVeryDarkness <3266343194@qq.com>
2025-01-22 20:38:10 +0800

    C passed

--- compliex/handin/filter.F90
+++ compliex/handin/filter.F90
@@ -1,24 +1,24 @@
 
 subroutine filter_run(x, wgt, ngrid, is, ie, js, je) bind(c)
     implicit none
-    double precision, dimension(2700, 113), intent(inout) :: x
-    double precision, dimension(1799, 113), intent(in) :: wgt
-    integer, dimension(113), intent(in) :: ngrid
+    double precision, dimension(0:2699, 0:112), intent(inout) :: x
+    double precision, dimension(0:1798, 0:112), intent(in) :: wgt
+    integer, dimension(0:113), intent(in) :: ngrid
     integer, value :: is, ie, js, je
-    double precision :: tmp(ie - is + 1)
+    double precision :: tmp(0:ie - is)
     integer :: j, i, p, n, hn
 
     do j = js, je
         n = ngrid(j)
         hn = (n - 1) / 2
         do i = is, ie
-            tmp(i - is + 1) = 0.0d0
+            tmp(i - is) = 0.0d0
             do p = 0, n - 1
-                tmp(i - is + 1) = tmp(i - is + 1) + wgt(p + 1, j) * x(i - hn + p + 1, j)
+                tmp(i - is) = tmp(i - is) + wgt(p, j) * x(i - hn + p, j)
             end do
         end do
         do i = is, ie
-            x(j, i) = tmp(i - is + 1)
+            x(i, j) = tmp(i - is)
         end do
     end do
 end subroutine filter_run
