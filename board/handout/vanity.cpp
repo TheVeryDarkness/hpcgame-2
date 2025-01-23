c240a46c678cc256258457ea94aec6d59f42eb77
TheVeryDarkness <3266343194@qq.com>
2025-01-23 16:21:20 +0800

    Update

--- board/handout/vanity.cpp
+++ board/handout/vanity.cpp
@@ -4,11 +4,15 @@
 #include <cstring>  
 #include <fstream>  
 #include <random>  
+#include <mutex>
+#include <shared_mutex>
 #include <openssl/sha.h>  
 #include <openssl/evp.h>  
 #include <secp256k1.h>  
+
+using engine = std::ranlux48_base;
   
-std::string toHex(const uint8_t* data, size_t size) {  
+static inline std::string toHex(const uint8_t* data, size_t size) {  
     std::ostringstream oss;  
     for (size_t i = 0; i < size; ++i) {  
         oss << std::hex << std::setfill('0') << std::setw(2) << (int)data[i];  
@@ -16,7 +20,7 @@ std::string toHex(const uint8_t* data, size_t size) {
     return oss.str();  
 }  
    
-std::string sha3256(const uint8_t* data, size_t size) {  
+static inline std::string sha3256(const uint8_t* data, size_t size) {  
     EVP_MD_CTX* context = EVP_MD_CTX_new();  
     const EVP_MD* md = EVP_get_digestbyname("sha3-256");  
     EVP_DigestInit_ex(context, md, nullptr);  
@@ -30,9 +34,15 @@ std::string sha3256(const uint8_t* data, size_t size) {
     return toHex(hash, hashLen);  
 }  
 
-void generateRandomPrivateKey(uint8_t privateKey[32], std::mt19937_64 &gen) {  
-    for (uint_fast64_t *p = (uint_fast64_t *)privateKey; p < (uint_fast64_t *)(privateKey + 32); ++p) {
-        *p = gen();
+static inline void generateRandomPrivateKey(uint8_t privateKey[32], engine &gen) {  
+    for (uint8_t *p = (uint8_t *)privateKey; p < privateKey + 32; p += 6) {
+        const uint_fast64_t r = { gen() };
+        p[0] = r >> 40;
+        p[1] = r >> 32;
+        p[2] = r >> 24;
+        p[3] = r >> 16;
+        p[4] = r >> 8;
+        p[5] = r;
     }
     // FILE* urandom = fopen("/dev/urandom", "rb");  
     // int res = fread(privateKey, 1, 32, urandom);  
@@ -43,7 +53,7 @@ void generateRandomPrivateKey(uint8_t privateKey[32], std::mt19937_64 &gen) {
     // fclose(urandom);  
 }  
 
-std::string computeEthereumAddress(const secp256k1_context* ctx, const uint8_t privateKey[32]) {  
+static inline std::string computeEthereumAddress(const secp256k1_context* ctx, const uint8_t privateKey[32]) {  
     secp256k1_pubkey pubkey;  
     secp256k1_ec_pubkey_create(ctx, &pubkey, privateKey);
     uint8_t pubkeySerialized[65];  
@@ -54,29 +64,49 @@ std::string computeEthereumAddress(const secp256k1_context* ctx, const uint8_t p
 
     return "0x" + hash.substr(24);  
 }  
+
+constexpr int MAX_THREADS = 8;
+constexpr int MAX_VANITY_LENGTH = 10;
   
 std::random_device rd;
-std::string vanityPrefixes[10];
-uint8_t privateKeys[10][32];
-std::string addresses[10];
+std::string vanityPrefixes[MAX_VANITY_LENGTH];
+uint8_t privateKeys[MAX_VANITY_LENGTH][32];
+std::string addresses[MAX_VANITY_LENGTH];
+std::shared_mutex mtx[MAX_VANITY_LENGTH];
+bool found[MAX_VANITY_LENGTH] = {};
 
 void *run(void *arg) {
-    const int i = *(int *)arg;
-    std::clog << "Running " << i << '\n';
-    std::mt19937_64 gen(rd());
+    const int init = *(int *)arg;
+    int i = init;
+    std::clog << "Started thread " << i << '\n';
+    engine gen(rd());
 
     secp256k1_context* ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN); 
 
-    const std::string &vanityPrefix = vanityPrefixes[i];
-    uint8_t* const privateKey = privateKeys[i];
-    std::string &address = addresses[i];
-
     while (true) {  
+        {
+            std::shared_lock lock(mtx[i]);
+            if (found[i]) {
+                i = (i + 1) % MAX_VANITY_LENGTH;
+                if (i == init) {
+                    break;
+                }
+                continue;
+            } 
+        }
+        const std::string &vanityPrefix = vanityPrefixes[i];
+        uint8_t* const privateKey = privateKeys[i];
+        std::string &address = addresses[i];
+
         generateRandomPrivateKey(privateKey, gen);  
         address = computeEthereumAddress(ctx, privateKey);  
         if (address.substr(2, vanityPrefix.size()) == vanityPrefix) {  
-            std::clog << "Exited " << i << '\n';
-            break;  
+            std::clog << "Found vanity " << i << '\n';
+            i = (i + 1) % MAX_VANITY_LENGTH;
+            {
+                std::unique_lock lock(mtx[i]);
+                found[i] = true;
+            }
         }  
     }  
     secp256k1_context_destroy(ctx);  
@@ -87,23 +117,23 @@ int main(int argc, char* argv[]) {
     std::ifstream infile("vanity.in");
     std::ofstream outfile("vanity.out");
 
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < MAX_VANITY_LENGTH; ++i) {
         infile >> vanityPrefixes[i];
     }
 
-    pthread_t threads[10];
-    int args[10];
+    pthread_t threads[MAX_THREADS];
+    int args[MAX_THREADS];
 
-    for(int i = 0; i < 10; ++i){
+    for(int i = 0; i < MAX_THREADS; ++i){
         args[i] = i;
         pthread_create(&threads[i], nullptr, run, args + i);
     }
 
-    for(int i = 0; i < 10; ++i){
+    for(int i = 0; i < MAX_THREADS; ++i){
         pthread_join(threads[i], nullptr);
     }
 
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < MAX_VANITY_LENGTH; ++i) {
         const uint8_t* const privateKey = privateKeys[i];
         std::string &address = addresses[i];
 
