b7b0ed4373ad541d2d092f961f54ae5a6cc37384
TheVeryDarkness <3266343194@qq.com>
2025-01-24 22:17:13 +0800

    Update

--- genshin-prof.sh
+++ genshin-prof.sh
@@ -1,8 +1,11 @@
 set -eux
 
-g++ -std=c++17 -pg -Og -march=native genshin.cpp -o genshin
-gprof -b ./program >report.txt
-python3 genshin-check.py
+g++ -std=c++17 -pg -g -Og -march=native program.cpp -o program
+OMP_NUM_THREADS=2 ./program
+diff out.data out.data.std
+gprof -A -B -C -p -l -I . ./program >./report.txt
+
+# python3 genshin-check.py
 
 prof2dot -f gprof report.txt | dot -Tpng -o output.png
 
